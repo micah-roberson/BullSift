d790e0ea0af38282513594b4c858b56f4dd0f7d5
Yaser <yayonoss@gmail.com>
2024-09-14 02:57:47 -0400

    Added CORS

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/generate', async (req, res) => {
